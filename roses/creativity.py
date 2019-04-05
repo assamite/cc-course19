9fedfea02039c3bed84bb950a56a7312177e9325
Ville-Veikko Saari <viltsus@gmail.com>
2019-04-05 19:14:54 +0300

    typo fix

--- roses/creativity.py
+++ roses/creativity.py
@@ -2,5 +2,5 @@ from typing import List
 
 
 def creativity(emotion: str, rhyming_partials: List[object]):
-    return [['Roses are red', 'violets are sad', 'this project is not done', 'and you should be clad']
+    return [['Roses are red', 'violets are sad', 'this project is not done', 'and you should be glad']
             for _ in range(1000)]
