0647c657af8b6b62812ba2685c1cfba5a9e8197f
Tuomas Siipola <siiptuo@kapsi.fi>
2019-04-12 15:49:56 +0300

    Larger score for results found in multiple categories

--- tittles/thesaurus.py
+++ tittles/thesaurus.py
@@ -3,14 +3,17 @@ import xml.etree.ElementTree as ET
 from collections import Counter
 
 def fetch(category, adjectives):
-    counter = Counter()
+    weights = Counter()
+    counts = Counter()
     for adjective in adjectives:
         r = requests.get('http://ngrams.ucd.ie/therex3/common-nouns/category.action', params={'cate': f'{adjective}:{category}', 'xml': 'true'})
         root = ET.fromstring(r.text)
         members = {member.text.strip(): int(member.attrib['weight']) for member in root.iter('Member')}
-        members = {k: v / max(members.values()) / len(adjectives) for k, v in members.items()}
-        counter.update(members)
-    return counter
+        members = {k: v / max(members.values()) for k, v in members.items()}
+        weights.update(members)
+        counts.update(members.keys())
+    members = weights.keys()
+    return {m: ((counts[m] - 1) / (len(adjectives) - 1) + weights[m] / len(adjectives)) / 2 for m in members}
 
 if __name__ == "__main__":
     import sys
@@ -25,7 +28,7 @@ if __name__ == "__main__":
     print(' adjectives:', ', '.join(adjectives))
     print(' suggestions:')
     suggestions = fetch('person', adjectives)
-    for suggestion in suggestions.most_common(10):
+    for suggestion in Counter(suggestions).most_common(10):
         print(f'  {suggestion[1]:.2f} {suggestion[0]}')
 
     adjectives = list(word_pair[1] for word_pair in word_pairs if word_pair[0] == 'animal')
@@ -33,5 +36,5 @@ if __name__ == "__main__":
     print(' adjectives:', ', '.join(adjectives))
     print(' suggestions:')
     suggestions = fetch('animal', adjectives)
-    for suggestion in suggestions.most_common(10):
+    for suggestion in Counter(suggestions).most_common(10):
         print(f'  {suggestion[1]:.2f} {suggestion[0]}')
