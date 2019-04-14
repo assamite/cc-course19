390a7129a3f5a7d1e6d9461b10520e697ca32408
kzx <samsamsamrox1212@gmail.com>
2019-04-14 22:52:30 +0300

    Optimized GAN module and added GPU toggling

--- gpri/gpri_helper/model.py
+++ gpri/gpri_helper/model.py
@@ -9,26 +9,21 @@ import tensorflow_hub as hub
 import numpy as np
 from scipy.stats import truncnorm
 
-tf.reset_default_graph()
 
-module_path = 'https://tfhub.dev/deepmind/biggan-deep-256/1'
+module_path = 'https://tfhub.dev/deepmind/biggan-128/2'
 print ('Loading BigGAN module.........')
 module = hub.Module(module_path)
 print ('BigGAN module loaded!')
-
-inputs = {k: tf.placeholder(v.dtype, v.get_shape().as_list(), k) for k, v in module.get_input_info_dict().items()}
+inputs = {k: tf.placeholder(v.dtype, v.get_shape().as_list(), k)
+          for k, v in module.get_input_info_dict().items()}
 output = module(inputs)
-
+        
 input_z = inputs['z']
 input_y = inputs['y']
 input_trunc = inputs['truncation']
 
 dim_z = input_z.shape.as_list()[1]
 vocab_size = input_y.shape.as_list()[1]
-
-initializer = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(initializer)
 
 def truncated_z_sample(batch_size, truncation=1., seed=None):
   state = None if seed is None else np.random.RandomState(seed)
@@ -52,7 +47,7 @@ def one_hot_if_needed(label, vocab_size=vocab_size):
   assert len(label.shape) == 2
   return label
 
-def sample(noise, label, truncation=1., batch_size=8,
+def sample(sess, noise, label, truncation=1., batch_size=8,
            vocab_size=vocab_size):
   noise = np.asarray(noise)
   label = np.asarray(label)
@@ -73,3 +68,5 @@ def sample(noise, label, truncation=1., batch_size=8,
   ims = np.clip(((ims + 1) / 2.0) * 256, 0, 255)
   ims = np.uint8(ims)
   return ims
+
+
--- gpri/main.py
+++ gpri/main.py
@@ -6,13 +6,23 @@ Contains initialize- and create-functions.
 import random
 import os
 from io import BytesIO
-#import IPython.display
+import IPython.display
 import numpy as np
+import random
 import PIL.Image
+from scipy.stats import truncnorm
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
 import tensorflow_hub as hub
-from gpri.gpri_helper import model
 import cv2
+import time
+
+GPU_MODE = False
+
+
+animal_idxs =[i for i in range(0,398)]
+activity_idxs =[i for i in range(398,1000)]
+
 
 class RandomImageCreator:
 
@@ -23,31 +33,58 @@ class RandomImageCreator:
         Only keyword arguments are supported in config.json
         """
         print("Group GPRI initialize.")
-        self.dims = kwargs.pop('resolution', [100, 100])
-        self.folder = os.path.dirname(os.path.realpath(__file__))
-
-        #tf.reset_default_graph()
-
-
-        #initializer = tf.global_variables_initializer()
-        #self.sess = tf.Session()
-        #self.sess.run(initializer)
 
         # Each creator should have domain specified: title, poetry, music, image, etc.
         self.domain = 'image'
+        self.dims = kwargs.pop('resolution', [100, 100])
+        self.folder = os.path.dirname(os.path.realpath(__file__))
+        self.sess = None
+        choice='N'
+        choice = input("Enable GPU mode (Y/n)? Select N if you don't have a GPU. ")
+        while True:
+            if choice == 'N' or choice == 'n':
+                GPU_MODE=False
+                print('CPU mode enabled, loading dummy image.....')
+                break
+            elif choice == 'Y' or choice == 'y':
+                GPU_MODE=True
+                print('GPU mode enabled..\n')
+                break
+            else:
+                print('Try again (Invalid input).')
+        if GPU_MODE:
+            global graph,model
+            tf.reset_default_graph()
+            graph = tf.get_default_graph()
+            from .gpri_helper import model
+            initializer = tf.global_variables_initializer()
+            self.sess = tf.Session()
+            self.sess.run(initializer)
+        
 
-    def generate(self, *args, **kwargs):
+    def generate(self, idx, *args,**kwargs):
         """Random image generator.
         """
-        truncate=0.8
-        noise=0
-        n_samples=1
-        idx_cat = args[0]
-        z = model.truncated_z_sample(n_samples, 0.8, noise)
-        y = idx_cat
-        ims = model.sample(z, y, 0.8)
-        cv2.imwrite("gpri/broken_sample.jpg", ims[0])
-        return os.path.join(self.folder, "broken_sample.jpg")
+        PATH = str(self.folder)
+        NAME = str("GPRI_%s.png" % (time.time()))
+            
+        if GPU_MODE:
+            truncate=0.8
+            noise=0
+            n_samples=1
+            print(idx)
+            idx_cat = idx
+            with graph.as_default():
+                z = model.truncated_z_sample(n_samples, truncate, noise)
+                y = idx_cat
+                ims = model.sample(self.sess, z, y, truncate)
+            print('Saving image.....')
+            image = cv2.cvtColor(ims[0], cv2.COLOR_BGR2RGB)
+            cv2.imwrite(f"{PATH}\{NAME}", image)
+        else:
+            return (os.path.join(PATH, "babylon_drawing.jpg"))
+        return os.path.join(PATH, NAME)
+
 
     def evaluate(self, image):
         """Evaluate image. For now this is a dummy.
@@ -83,23 +120,21 @@ class RandomImageCreator:
 
         """
         print("Group Example create with input args: {} {}".format(emotion, word_pairs))
-        with open("gpri/categories.txt", "r") as file:
+        with open(f"{self.folder}\categories.txt", "r") as file:
             buffer = file.read()
             cat = buffer.split("\n")
-        cat_indx = []
-        for pairs in word_pairs[1]:
-            noun = pairs
+        for pairs in word_pairs:
+            noun = pairs[0]
             print(noun)
             if noun == "animal":
-                idxs =[i for i in range(0,398)]
+                idx = int(random.choice(animal_idxs))    
             elif noun == "activity":
-                idxs =[i for i in range(398,1000)]
+                idx = int(random.choice(activity_idxs))
             else:
-                idxs = -1
-            if idxs == -1:
-                print("Category not available yet as, category annotation is pending.... (Try animal/activity only)")
-            else:
-                import random
-                idx_cat = int(random.choice(idxs))
-            ret = [(w, {'evaluation': self.evaluate(w)}) for w in [self.generate(idx_cat) for _ in range(number_of_artifacts)]]
-        return ret
+                idx = -1 #CheeseBurger
+            break #taking the first pair for now
+        if idx == -1:
+            print("Category not available yet as, category annotation is pending, returning yummy cheeseburger.... (Try animal/activity only)")
+            ret = [(w, {'evaluation': self.evaluate(w)}) for w in [self.generate(933) for _ in range(number_of_artifacts)]]
+        else:
+            ret = [(w, {'evaluation': self.evaluate(w)}) for w in [self.generate(idx) for _ in range(number_of_artifacts)]]
