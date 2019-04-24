f8729bd381572e26aeec2a41945e93e60e074cd5
Rämö <miiaramo@lm7-cs-46.local>
2019-04-24 14:57:45 +0300

    verb converted to singular form

--- roses/alter_word_pairs.py
+++ roses/alter_word_pairs.py
@@ -5,4 +5,4 @@ def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
     """
     Generates a bunch of word pairs depending on input word pairs and emotion.
     """
-    return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'be'} for word_pair in word_pairs]
+    return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'is'} for word_pair in word_pairs]
