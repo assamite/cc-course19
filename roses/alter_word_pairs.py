a2b114c75974306eebaefc894cc669866ee26590
Vili <vili.hatonen@helsinki.fi>
2019-04-24 15:05:15 +0300

    moved modules under modules/ folder

--- roses/alter_word_pairs.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Tuple
-
-
-def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
-    """
-    Generates a bunch of word pairs depending on input word pairs and emotion.
-    """
-    return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'is'} for word_pair in word_pairs]
