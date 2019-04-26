80b910f251721d667bc8a750d877cd1f384767ba
afhuertass <afhuertass@unal.edu.co>
2019-04-26 09:47:01 +0300

    Created style.py for style selection, so far it picks a folder style at random and then a particular style at random, with uniform probabilities en both cases

--- group_picasso/style.py
+++ group_picasso/style.py
@@ -27,11 +27,11 @@ def load_probabilities():
 
 
 def get_subdirs(dir):
-    "Get a list of immediate subdirectories"
+
     return next(os.walk(dir))[1]
 
 def get_subfiles(dir):
-    "Get a list of immediate subfiles"
+
     return next(os.walk(dir))[2]
 
 def select_style( path ,emotion ):
