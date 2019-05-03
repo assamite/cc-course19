b3ab0896a7cefdec864c392befec07fd71dc8114
Dobrodeev <vladimir.dobrodeev.ext@nokia.com>
2019-05-03 16:08:29 +0300

    Added comments to create-doodle.sh

--- random_team/api/create-doodle.sh
+++ random_team/api/create-doodle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# alias doodle="docker run -v $(pwd)/samples:/nd/samples -v $(pwd)/frames:/nd/frames -it neural-doodle-cc-19"
+# Runs Neural doodle to create a doodle from provided content and style images
+# Neural doodle can not run in detached mode so there is no way to have a container in background
 docker run -v $(pwd)/samples:/nd/samples -v $(pwd)/frames:/nd/frames -it neural-doodle-cc-19 --style samples/style.png --content samples/face.png --output samples/result.png --device=cuda* --phases=4 --iterations=80
